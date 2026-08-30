c45febf4049af2e637de3970448462e79dd06f2d
abualiyan24-tech <abualiyan24@gmail.com>
2026-08-31 03:12:22 +0500

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,45 @@
+module.exports = {
+    // =================== BOT INFO ===================
+    botName: 'MA CRASHER',
+    botOwner: 'Muhammad Ayan',
+    teamName: 'MA Developers',
+    version: '2.0.0',
+    prefix: '.',
+    
+    // =================== IMAGES ===================
+    menuImage: 'https://i.postimg.cc/W3MK0wgJ/Chat-GPT-Image-Aug-26-2026-03-06-36-PM.png',
+    ownerImage: 'https://i.postimg.cc/050FQZ89/Chat-GPT-Image-Aug-26-2026-03-07-12-PM.png',
+    startImage: 'https://i.postimg.cc/W3MK0wgJ/Chat-GPT-Image-Aug-26-2026-03-06-36-PM.png',
+    
+    // =================== CONTACT LINKS ===================
+    whatsappChannel: 'https://whatsapp.com/channel/0029VaGiJKfIiRoybPBMTy38',
+    youtubeChannel: 'https://youtube.com/@MADevelopers',
+    instagram: 'https://instagram.com/ma_developers',
+    facebook: 'https://facebook.com/ma_developers',
+    telegram: 'https://t.me/ma_developers',
+    github: 'https://github.com/ma-developers',
+    website: 'https://ma-developers.com',
+    
+    // =================== OWNER CONTACT ===================
+    ownerNumber: '923000000000',
+    ownerEmail: 'ma.developers@gmail.com',
+    
+    // =================== BIO ===================
+    botBio: '⚡ MA CRASHER - Ultimate WhatsApp Bot\n© MA Developers | Muhammad Ayan',
+    
+    // =================== FOOTER ===================
+    footer: '© MA Developers',
+    copyright: 'Muhammad Ayan',
+    
+    // =================== PREMIUM USERS ===================
+    premiumUsers: [],
+    
+    // =================== CONNECTED BOTS ===================
+    connectedBots: [],
+    
+    // =================== DEFAULT SETTINGS ===================
+    autoRead: false,
+    autoStatus: false,
+    publicMode: true,
+    antilink: false
+};
